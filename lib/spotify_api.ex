83b7882dacf159d9c5589ff042e93593b501737b
kamil <frankiewiczkamil@gmail.com>
2022-03-21 22:52:17 +0100

    add basic episodes fetching :construction: fetch episodes by show id

--- lib/spotify_api.ex
+++ lib/spotify_api.ex
@@ -1,8 +1,13 @@
 defmodule Spotify_API do
   # dev mode only, tmp
+  @api "https://api.spotify.com/v1"
   @headers [Authorization: "Bearer #{Auth.get_dev_token()}"]
 
   def get_my_shows_factory() do
-    HTTPoison.get!("https://api.spotify.com/v1/me/shows", @headers)
+    HTTPoison.get!("#{@api}/me/shows", @headers)
+  end
+
+  def get_episodes_by_show_id(show_id) do
+    HTTPoison.get!("#{@api}/shows/#{show_id}/episodes", @headers)
   end
 end
