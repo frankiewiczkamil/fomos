47cf883e46995851ecefcd9f770f4493416f6ade
kamil <frankiewiczkamil@gmail.com>
2022-03-22 22:03:49 +0100

    basic parsing "show" endpoint response :alembic:

--- lib/spotify_api.ex
+++ lib/spotify_api.ex
@@ -3,8 +3,24 @@ defmodule Spotify_API do
   @api "https://api.spotify.com/v1"
   @headers [Authorization: "Bearer #{Auth.get_dev_token()}"]
 
-  def get_my_shows_factory() do
+  @type main_info :: %{
+          id: String.t(),
+          name: String.t()
+        }
+
+  defp pick_body(%{body: body}), do: body
+  defp pick_items(%{"items" => items}), do: items
+  defp pick_show(%{"show" => show}), do: show
+  defp to_main_info(%{"id" => id, "name" => name}), do: %{id: id, name: name}
+
+  @spec get_my_shows() :: [main_info]
+  def get_my_shows() do
     HTTPoison.get!("#{@api}/me/shows", @headers)
+    |> pick_body()
+    |> Jason.decode!()
+    |> pick_items()
+    |> Enum.map(&pick_show/1)
+    |> Enum.map(&to_main_info/1)
   end
 
   def get_episodes_by_show_id(show_id) do
