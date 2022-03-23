58e45259f54fdcb7036fc6285b51fae29973f5cb
kamil <frankiewiczkamil@gmail.com>
2022-03-23 21:15:39 +0100

    basic parsing "episodes" endpoint response :alembic:

--- lib/spotify_api.ex
+++ lib/spotify_api.ex
@@ -11,7 +11,11 @@ defmodule Spotify_API do
   defp pick_body(%{body: body}), do: body
   defp pick_items(%{"items" => items}), do: items
   defp pick_show(%{"show" => show}), do: show
-  defp to_main_info(%{"id" => id, "name" => name}), do: %{id: id, name: name}
+  defp show_main_info(%{"id" => id, "name" => name}), do: %{id: id, name: name}
+
+  defp episode_main_info(%{"release_date" => release_date, "name" => name}) do
+    %{release_date: release_date, name: name}
+  end
 
   @spec get_my_shows() :: [main_info]
   def get_my_shows() do
@@ -20,10 +24,14 @@ defmodule Spotify_API do
     |> Jason.decode!()
     |> pick_items()
     |> Enum.map(&pick_show/1)
-    |> Enum.map(&to_main_info/1)
+    |> Enum.map(&show_main_info/1)
   end
 
   def get_episodes_by_show_id(show_id) do
     HTTPoison.get!("#{@api}/shows/#{show_id}/episodes", @headers)
+    |> pick_body()
+    |> Jason.decode!()
+    |> pick_items()
+    |> Enum.map(&episode_main_info/1)
   end
 end
