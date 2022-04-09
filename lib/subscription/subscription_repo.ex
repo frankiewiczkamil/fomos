b47500ef2fff119eeaf4a4fe9a7b6e56bf451bf9
kamil <frankiewiczkamil@gmail.com>
2022-04-09 15:11:17 +0200

    add more methods to (tmp) subscription repo :construction:

--- lib/subscription/subscription_repo.ex
+++ lib/subscription/subscription_repo.ex
@@ -3,8 +3,29 @@ defmodule Subscription.Repo do
 
   @spec store(String.t(), list) :: any
   def store(id, shows) do
-    {:ok, table} = :dets.open_file(:"subscription.db", type: :set)
-    :dets.insert(table, {id, shows})
+    table = init()
+    :dets.insert(table, {id, shows, DateTime.utc_now()})
     :ok
+  end
+
+  def get(id) do
+    table = init()
+    :dets.lookup(table, id)
+  end
+
+  def first() do
+    table = init()
+    :dets.first(table)
+  end
+
+  def next(id) do
+    table = init()
+    :dets.lookup(table, id)
+  end
+
+  defp init() do
+    # todo change this module into genserver or sth like that
+    {:ok, table} = :dets.open_file(:"subscription.db", type: :set)
+    table
   end
 end
