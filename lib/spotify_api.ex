1ea814e0f6a6cecef68634dcbc7dc3c15a19ed23
kamil <frankiewiczkamil@gmail.com>
2022-04-18 16:26:58 +0200

    spotify pagination helper function :sparkles:

--- lib/spotify_api.ex
+++ lib/spotify_api.ex
@@ -11,6 +11,35 @@ defmodule Spotify_API do
     fetch_loop(url, authorization, [], transformation)
   end
 
+  def fetch(url, authorization, offset, limit) do
+    fetch("#{url}?limit=#{limit}&offset=#{offset}", authorization)
+  end
+
+  def create_pagination_parameters(elements_to_fetch, page_size) do
+    division = div(elements_to_fetch, page_size)
+    remainder = rem(elements_to_fetch, page_size)
+
+    rest =
+      case remainder do
+        0 -> []
+        _ -> [pagination_parameters(division * page_size, remainder)]
+      end
+
+    aliquotes =
+      case division do
+        0 ->
+          []
+
+        _ ->
+          Enum.map(1..division, fn x -> (x - 1) * page_size end)
+          |> Enum.map(fn offset -> %{offset: offset, limit: page_size} end)
+      end
+
+    aliquotes ++ rest
+  end
+
+  def pagination_parameters(offset, limit), do: %{offset: offset, limit: limit}
+
   def fetch(url, authorization) do
     HTTPoison.get!(url, Authorization: authorization)
     |> pick_body()
---
+++ test/spotify_api_test.exs
@@ -0,0 +1,45 @@
+defmodule Spotify_API_Test do
+  use ExUnit.Case
+  doctest Spotify_API
+
+  test "should create pagination for result = page_size" do
+    expected = [
+      %{offset: 0, limit: 5}
+    ]
+
+    result = Spotify_API.create_pagination_parameters(5, 5)
+    assert(expected == result)
+  end
+
+  test "should create pagination for result < page_size" do
+    expected = [
+      %{offset: 0, limit: 3}
+    ]
+
+    result = Spotify_API.create_pagination_parameters(3, 5)
+    assert(expected == result)
+  end
+
+  test "should create pagination for result > page_size + remain" do
+    expected = [
+      %{offset: 0, limit: 5},
+      %{offset: 5, limit: 5},
+      %{offset: 10, limit: 5},
+      %{offset: 15, limit: 2}
+    ]
+
+    result = Spotify_API.create_pagination_parameters(17, 5)
+    assert(expected == result)
+  end
+
+  test "should create pagination for result > page_size" do
+    expected = [
+      %{offset: 0, limit: 5},
+      %{offset: 5, limit: 5},
+      %{offset: 10, limit: 5}
+    ]
+
+    result = Spotify_API.create_pagination_parameters(15, 5)
+    assert(expected == result)
+  end
+end
