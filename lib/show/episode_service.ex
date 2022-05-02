31ebb5eafb24db97914ae05f769d446235d87550
kamil <frankiewiczkamil@gmail.com>
2022-05-02 22:13:29 +0200

    reduce data scope in get by date response :construction: it shall be human readable

--- lib/show/episode_service.ex
+++ lib/show/episode_service.ex
@@ -1,29 +1,45 @@
 defmodule Episode.Service do
   require Logger
 
-  # todo this will return list of ids
   def get_id_by_date(date) do
-    get_by_date(date, &pick_episode/1)
+    # todo this will return list of ids - requires storing id
+    get_by_date(date, &epoisodes_to_episode_ids/1)
+  end
+
+  defp epoisodes_to_episode_ids(episodes) do
+    episodes
+    |> Enum.map(&pick_episode/1)
   end
 
   def get_by_date(date) do
-    transformation = fn episode -> episode |> pick_episode() |> episode_with_show() end
-    get_by_date(date, transformation)
+    get_by_date(date, &epoisodes_to_grouped_episodes/1)
   end
 
+  defp epoisodes_to_grouped_episodes(episodes) do
+    episodes
+    |> Enum.map(&pick_episode/1)
+    |> Enum.group_by(&pick_show_id/1)
+    |> Map.to_list()
+    |> Enum.map(&to_human_readable/1)
+    |> Enum.into(%{})
+  end
+
+  @spec get_by_date(any, any) :: any
   def get_by_date(date, transformation) do
     case result = Episode.Repo.get_by_date(date) do
       {:error, reason} -> %{"error" => reason}
-      [_ | _] -> result |> Enum.map(transformation)
+      [_ | _] -> result |> transformation.()
     end
   end
 
   defp pick_episode({_date, episode}), do: episode
+  defp pick_show_id(%{show_id: show_id}), do: show_id
+  defp pick_name(%{name: name}), do: name
 
-  defp episode_with_show(%{show_id: show_id, name: name}) do
-    %{
-      show: Show.Repo.get_show_name(show_id),
-      name: name
+  defp to_human_readable({show_id, episodes}) do
+    {
+      show_id |> Show.Repo.get_show_name(),
+      episodes |> Enum.map(&pick_name/1)
     }
   end
 end
